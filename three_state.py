7be4391f28791d31929e4696065729864201c7d9
Corey Hayford <corey.hayford@vanderbilt.edu>
2016-03-10 18:41:50 -0600

    Model running -- output shows exponentially increasing populations

--- three_state.py
+++ three_state.py
@@ -18,14 +18,23 @@ def declare_parameters():
     
     ### Initial Conditions ###
     
-    Parameter("A_0", 0)
-    Parameter("B_0", 0)
-    Parameter("C_0", 0)
+    Parameter("A_0", 2000)
+    Parameter("B_0", 2000)
+    Parameter("C_0", 2000)
     
     ### Growth Parameters ###
-    Parameter("k_dip_A", -0.033)
-    Parameter("k_dip_B",  0.000)
-    Parameter("k_dip_C",  0.016)
+    #Parameter("k_dip_A", -0.033)
+    #Parameter("k_dip_B",  0.000)
+    #Parameter("k_dip_C",  0.016)
+    
+    Parameter("k_div_A", 0.033)
+    Parameter("k_div_B",  0.020)
+    Parameter("k_div_C",  0.056)
+    
+    Parameter("k_death_A", -0.066)
+    Parameter("k_death_B",  -0.020)
+    Parameter("k_death_C",  -0.040)
+    
     Parameter("k_AB",     0.025)
     Parameter("k_BA",     0.00004)
     Parameter("k_CB",     0.025)
@@ -51,16 +60,16 @@ def declare_observables():
 
 def declare_functions():
     
-    Rule('Divide_A', A >> A + A, k_dip_A)
-    Rule('Death_A',  A >> None,  k_dip_A)
-    Rule('Divide_B', B >> B + B, k_dip_B)
-    Rule('Death_B',  B >> None,  k_dip_B)
-    Rule('Divide_C', C >> C + C, k_dip_C)
-    Rule('Death_C',  C >> None,  k_dip_C)
+    Rule('Divide_A', A() >> A() + A(), k_div_A)
+    Rule('Death_A',  A() >> None,  k_death_A)
+    Rule('Divide_B', B() >> B() + B(), k_div_B)
+    Rule('Death_B',  B() >> None,  k_death_B)
+    Rule('Divide_C', C() >> C() + C(), k_div_C)
+    Rule('Death_C',  C() >> None,  k_death_C)
     
-    Rule("A_to_B", A >> B,       k_AB)
-    Rule("B_to_A", B >> A,       k_BA)
-    Rule("B_to_C", B >> C,       k_BC)
-    Rule("C_to_B", C >> B,       k_CB)
+    Rule("A_to_B", A() >> B(),       k_AB)
+    Rule("B_to_A", B() >> A(),       k_BA)
+    Rule("B_to_C", B() >> C(),       k_BC)
+    Rule("C_to_B", C() >> B(),       k_CB)
        
     
--- three_state_run.py
+++ three_state_run.py
@@ -4,10 +4,13 @@ from pysb.macros import *
 from pysb.bng import *
 from pysb.core import *
 from pysb.integrate import odesolve
-#import pylab as pl
+from pylab import *
+import pylab as pl
 from numpy import linspace
 from sympy import sympify
 from scipy import constants 
+import matplotlib.pyplot as plt
+
 
  
 import three_state
@@ -42,4 +45,38 @@ print len(model.rules)
 print len(model.initial_conditions)
 print len(model.reactions)
 print len(model.species)
-quit()
+
+#quit()
+
+
+t = linspace(0,8000, 800)
+
+y = odesolve(model,t,verbose=True)
+
+plt.figure()
+for obs in ["Obs_A", "Obs_B", "Obs_C"]:
+    plt.plot(t, y[obs], label=re.match(r"Obs_(\w+)", obs).group(1), linewidth=3)
+plt.legend(loc=0, prop={'size': 16})
+plt.xlabel("Time", fontsize=22)
+plt.ylabel("Cell Population", fontsize=22)
+plt.xticks(fontsize=18)
+plt.yticks(fontsize=18)
+plt.title("Three-State Model", fontsize=22)
+
+plt.show()
+quit() 
+ 
+plt.figure()
+
+for obs in ["Obs_A", "Obs_B", "Obs_C"]:
+    plt.plot(t, y[obs], label=re.match(r"Obs_(\w+)", obs).group(1), linewidth=3)
+plt.legend(loc=0, prop={'size': 16})
+plt.xlabel("Time", fontsize=22)
+plt.ylabel("Cell Population", fontsize=22)
+plt.xticks(fontsize=18)
+plt.yticks(fontsize=18)
+plt.title("Three-State Model", fontsize=22)
+
+plt.show()
+
+#pl.savefig("three_state_model", format= "png")
