02ba0836eedacca888ef7adaa7ec2d474abcb348
Corey Hayford <corey.hayford@vanderbilt.edu>
2016-06-02 00:52:16 -0500

    Using Scipy.optimize's minimize function, estimated parameters for 3 state model (using DIP rates and different division/death rates - three_state_dip.py and three_state.py respectively)

--- three_state_dip.py
+++ three_state_dip.py
@@ -23,17 +23,17 @@ def declare_parameters():
     Parameter("C_0", 1000)
     
     ### Growth Parameters ###
-    #Parameter("k_dip_A", -0.033)
-    #Parameter("k_dip_B",  0.000)
-    #Parameter("k_dip_C",  0.016)
+    Parameter("k_dip_A", -0.033)
+    Parameter("k_dip_B",  0.000)
+    Parameter("k_dip_C",  0.016)
     
-    Parameter("k_div_A", 0.033)
-    Parameter("k_div_B",  0)
-    Parameter("k_div_C",  0.016)
-    
-    Parameter("k_death_A", 0.066)
-    Parameter("k_death_B",  0)
-    Parameter("k_death_C",  0)
+#     Parameter("k_div_A", 0.033)
+#     Parameter("k_div_B",  0)
+#     Parameter("k_div_C",  0.016)
+#     
+#     Parameter("k_death_A", 0.066)
+#     Parameter("k_death_B",  0)
+#     Parameter("k_death_C",  0)
     
     Parameter("k_AB",     0.025)
     Parameter("k_BA",     0.00004)
@@ -60,12 +60,16 @@ def declare_observables():
 
 def declare_functions():
     
-    Rule('Divide_A', A() >> A() + A(), k_div_A)
-    Rule('Death_A',  A() >> None,  k_death_A)
-    Rule('Divide_B', B() >> B() + B(), k_div_B)
-    Rule('Death_B',  B() >> None,  k_death_B)
-    Rule('Divide_C', C() >> C() + C(), k_div_C)
-    Rule('Death_C',  C() >> None,  k_death_C)
+#     Rule('Divide_A', A() >> A() + A(), k_div_A)
+#     Rule('Death_A',  A() >> None,  k_death_A)
+#     Rule('Divide_B', B() >> B() + B(), k_div_B)
+#     Rule('Death_B',  B() >> None,  k_death_B)
+#     Rule('Divide_C', C() >> C() + C(), k_div_C)
+#     Rule('Death_C',  C() >> None,  k_death_C)
+    
+    Rule('DIP_A', A() >> A() + A(), k_dip_A)
+    Rule('DIP_B', B() >> B() + B(), k_dip_B)
+    Rule('DIP_C', C() >> C() + C(), k_dip_C)
     
     Rule("A_to_B", A() >> B(),       k_AB)
     Rule("B_to_A", B() >> A(),       k_BA)
