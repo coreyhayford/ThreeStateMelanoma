ecff7f44c2450d270303e0c5cddedb299d1e87cd
Corey Hayford <corey.hayford@vanderbilt.edu>
2016-06-14 17:05:07 -0500

    Test commit.

--- testPE.py
+++ testPE.py
@@ -45,6 +45,9 @@ three_state.declare_initial_conditions()
 three_state.declare_observables()
 three_state.declare_functions()
 
+
+
+
 ####################################################################################################
 ### Experimental Data Cleaning and Plotting
 ####################################################################################################
@@ -86,7 +89,7 @@ for i,m in enumerate(mixing):
 
     nl2_average.append((nl2_rep1 + nl2_rep2 + nl2_rep3) / 3.0)
     
-    plt.figure(i)
+    plt.figure()
     
     plt.plot(time, nl2_average[-1], ms = 0.50, mfc = "0.25")
     
@@ -97,9 +100,9 @@ for i,m in enumerate(mixing):
     
 nl2_average = np.array(nl2_average) # Make nl2_average list an array - easier to work with
 
-### Define new initial conditions ###
+### Define other initial conditions ###
 # Function that creates an empty list that is appended with initial cell counts and returns
-# that list. This list will be used to continuously change simulation ICs
+# that list. This list will be used to continuously change simulation ICs.
 def ICs():
     ics = []
     ics.append([600, 1800, 600]) #parental
@@ -129,23 +132,27 @@ for i, ic in enumerate(ICs()):
     plt.figure(i)
     plt.plot(time, nl2_sim, 'ro-', ms = 12, mfc = "None", mew = 2, mec = "r")
 #plt.show()
-   
+
+####################################################################################################
+### Parameter Estimation
+####################################################################################################
+
 ### Score Function ###
 param_values = np.array([p.value for p in model.parameters]) # Do first so can manipulate in function
-#
 def score(params):
-    #13 parameters now
-    #params = [kdipA, kdipB, kdipB, kAB, kBA, kBC, kCB, 
+    # k_value parameters (7) + Initial conditions parameters (3 * #conditions)
+    # ex: params = [kdipA, kdipB, kdipB, kAB, kBA, kBC, kCB, 
     #          A_0[0], B_0[0], C_0[0], A_0[1], B_0[1], C_0[1], ...]
-    #eq changed to solver.run
-    params = np.array(params)
-    param_values = np.zeros(len(model.parameters))
-    #param_values = [A_0, B_0, C_0, kdipA, kdipB, kdipB, kAB, kBA, kBC, kCB]
-    k_params = params[:7]
-    param_values[3:] = k_params
-    init_params =  params[7:]
-    n_sims = len(init_params) / 3 #integer
-    sse = 0
+    params = np.array(params) # change list of params to array
+    param_values = np.zeros(len(model.parameters)) # fill param_values object with an array of zeros (lenght of number of parameters)
+    # ex: param_values = [A_0, B_0, C_0, kdipA, kdipB, kdipB, kAB, kBA, kBC, kCB]
+    k_params = params[:7] # the first 7 parameters (index 0-6) are rate parameters
+    param_values[3:] = k_params # replace 0s in param_values list with rate paramters (placed after ICs)
+    init_params =  params[7:] # the final n*3 parameters - different ICs
+    n_sims = len(init_params) / 3 # define number of simulations by number of ICs divided by 3 (ICs per simulation) - integer
+    sse = 0 # set initial sum-of-squared errors to 0
+    # Loop over each simulation to overwrite IC param_values with different ICs, olve ODE for those params, normalize, and return the 
+    # SSE cost (which is additive for each sequential (new IC) simulation
     for n in range(n_sims):
         start = n*3
         end = start+3
@@ -155,56 +162,43 @@ def score(params):
         sse += ((nl2_average[n] - nl2_sim)**2).sum() # Sum of squared errors - cost function
     return sse
 
-#print model.parameters
-#print model.parameters[0].value # or name
-#fit_score=score([p.value for p in model.parameters[3:-1]]) # this is for three_state (non-dip)
 #fit_score=score([p.value for p in model.parameters[:]]) # No indecies reported so all parameters can float (including initial conditions)
 #print fit_score
 
-### Optimize Fit of parameters ###
-
-#x0 = np.array([p.value for p in model.parameters])[3:-1] # this is for three_state (non-dip)
-x0 = [p.value for p in model.parameters[3:]] # No indecies reported so all parameters can float (including initial conditions)
+### Making parameter initial guess list ###
+# Store rate parameter values in a list that is appended (at the end) with chosen ICs (in groups of 3) additively
+x0 = [p.value for p in model.parameters[3:]]
 for ic in ICs():
     x0 += ic
 
-print len(x0)
-print x0
-
-
-#
-#print x0
-#print len(x0)
-#bounds = [(0,None)]*len(model.parameters[3:-1])
+### Setting parameter bounds ###
+# Store bounds in a list of rate parameters that is appended by initial IC guesses that can vary +/- bound_variance
+# Some parameters are set to 0 - so the variance does not affect those
 bounds = [(None, 0), (-0.5, 0.5), (0, None), (0, None), (0, None), (0, None), (0, None)]
+bound_variance = 0.1 # 10% variance
 for i in range(len(bounds), len(x0)):
-    bounds.append((x0[i]-0.1*x0[i], x0[i]+0.1*x0[i]))
- # hard coded - should change
-#print len(bounds)
-
+    bounds.append((x0[i]-bound_variance*x0[i], x0[i]+bound_variance*x0[i]))
+ 
+### Minimization of parameters ###
+## Parameters (param_values) are minimized over the cost function (sse) using 
+## an inital parameter guess (x0) with bounded constraints (bounds) and a max
+## number of iterations -- FOR CONSTRAINED OPTIMIZATION
 # Method L-BFGS-B works by identifying fixed and free variables at every step 
 # (using a simple gradient method), and then using the L-BFGS method on the free
 # variables only to get higher accuracy, and then repeating the process.
 # See https://en.wikipedia.org/wiki/Limited-memory_BFGS#L-BFGS-B for more details
-# FOR CONSTRAINED OPTIMIZATION
 result = minimize(score,x0, method = 'L-BFGS-B', bounds = bounds, options = {'maxiter':1000000})
-#for i,p in enumerate(model.parameters[3:-1]): # this is for three_state (non-dip)
-#    print p,result.x[i] # this is for three_state (non-dip)
 
+### New parameter determination (rates and ICs) ###
+# Store rate parameters results in an empty list that is appended iteratively with the best fit parameters from that simulation
 k_result = []
-for i,p in enumerate(model.parameters[3:]): # No indecies reported so all parameters can float (including initial conditions)
-    print p,result.x[i]
+for i,p in enumerate(model.parameters[3:]):
+    print p,result.x[i] # For visualization
     k_result.append(result.x[i])
     
-# ic_result = []
-# for i,p in enumerate(model.parameters[:3]):
-#     ic_result.append([])
-#     print p,
-#     for n in range(len(ICs())):
-#         print result.x[i+n*3+7],
-#         ic_result[-1].append(result.x[i+n*3+7])
-#     print
-
+# Store IC results in an empty list that is appended with an empty list for each iteration of ICs. The inner empty list is filled with the 
+# IC values from the previous simulation (or initial for first - index + IC list (1 list) * 3 (items in list) + 7 (after all rate parameters).
+# The previous result is then overwritten with the new parameters (using same equation above) - values are printed to screen
 ic_result = []
 for n in range(len(ICs())):
     ic_result.append([])
@@ -214,140 +208,19 @@ for n in range(len(ICs())):
         ic_result[-1].append(result.x[i+n*3+7])
     print "]"
 
-    
-
-
-# xlist = [1,2,3,4,5]
-# x_array = np.array(xlist)
-# ylist = [6,7,8,9,10,11,12]
-# y_array = np.array(ylist)
-# 
-# x_y_list = [x_array, y_array]
-# print x_y_list
-# print type(x_y_list)
-# x_y_array = np.array(x_y_list)
-# print x_y_array
-# print type(x_y_array)
-
- 
-#print xopt
-#print type(xopt)
-
-### New simulation
-
-#param_values[3:-1] = result.x
+### Plotting parameter optimized model output ###
+# Append a numpy array of 0s with rate parameters (last 7) and ICs (first 3). The rate parameters are the same for all the model outputs, 
+# while the ICs change. In each iteration, the parameter values are saved, normalized, and plotted.
 param_values = np.zeros(len(model.parameters))
 param_values[3:] = k_result
 for i, ic in enumerate(ICs()):
-    param_values[:3] = ic_result[i] # 2 lists that contain 3 numbers
+    param_values[:3] = ic_result[i] # N lists that contain 3 numbers
     solver.verbose = False
     solver.run(param_values=param_values) # saved in array - species y, obs in yobs, ic is 3 element list
     nl2_sim = np.log2(solver.yobs["Obs_All"]/solver.yobs["Obs_All"][0]) # making it nl2
     plt.figure(i)
     plt.plot(time, nl2_sim, 'go-', ms = 12, mfc = "None", mew = 2, mec = "g")
-
-
     plt.xlabel('Time')
     plt.ylabel('Population Doublings (nl2)')
     plt.title('Parameter Estimation')
 plt.show()
-
-
-### testing ideas ###
-
-
-# print x_array + y_array
-# print (x_array + y_array) / 2
-# print 3/2
-# print [(0,None)]*10
-
-
-#def mean(all_lists):
-#    return sum(all_lists) / len(all_lists)
-#all_lists = [nl2_rep1, nl2_rep2, nl2_rep3]
-#average_lists = map(mean, zip(*all_lists))
-#plt.plot(time, average_lists, ms = 12, mfc = "0.50")
-
-#for i in range(len(time)):
-#    print time[i], solver.yobs["Obs_All"][i]
-
-
-
-#sum_lists = [sum(i) for i in zip(*all_lists)]
-#print all_lists
-#print sum_lists
-#array = np.array([[nl2_rep1], [nl2_rep2], [nl2_rep3]])
-#rep_average = np.mean(array, axis = 0)
-#print rep_average
-#plt.plot(time, rep_average, ms = 12, mfc = "0.01")
-
-#def sublist(list, n):
-#    for i in range(1, len(list), n):
-#        yield list[i: i+n]
-
-#print sublist(time, 10)
-
-#exp_data = np.array([[time[i], nl2[i]] for i in range(1, len(time)) if time[i] < 10])
-#print exp_data
-
-#for i in range(1, len(time), 10):
-#    time_new = time[i, i+10],
-#    print time_new
-
-#def sublist(exp_data,10):
-#    sub = []; result = []
-#    for i in exp_data:
-#        sub+=[i]
-#        if len(sub)==n: result+=[sub]; sub =[]
-#    if sub:result += [sub]
-#    return result    
-        
-#cell_count = data_parental_treated["Cell.Nucleus"]
-#print cell_count
-
-# def eq(par,initial_cond,start_t,end_t,incr):
-#     # Create time space
-#     t  = np.linspace(start_t, end_t,incr)
-#     # Create ODE system
-#     def funct(y,t):
-#        A=y[0]
-#        B=y[1]
-#        C=y[2]
-#        par = k_div_A, k_div_B, k_div_C, k_death_A, k_death_B, k_death_C, k_AB, k_BA, k_CB, k_BC
-#        # the model equations from PySB model
-#        f0 = -A*k_AB - A*k_death_A + A*k_div_A + B*k_BA
-#        f1 = A*k_AB - B*k_BA - B*k_BC - B*k_death_B + B*k_div_B + C*k_CB
-#        f2 = B*k_BC - C*k_CB -C*k_death_C + C*k_div_C
-#        return [f0, f1, f2]
-     # Integrate over the equations
-#     ds = integrate.odeint(funct,initial_cond,t)
-#     print ds
-#     return (ds[:,0],ds[:,1],ds[:,2],t)
-
-#rates = (k_div_A, k_div_B, k_div_C, k_death_A, k_death_B, k_death_C, k_AB, k_BA, k_CB, k_BC)
-
-#### Define Initial Conditions ###
-#A_0 = 1000
-#B_0 = 1000
-#C_0 = 1000
-#y_0 = [A_0, B_0, C_0]
-
-#### Redefine model steps - encompassed in PySB model ###
-#start_time=0.0
-#end_time=120.0
-#intervals=10
-#mt=np.linspace(start_time,end_time,intervals)
-
-#### Model index to compare to data ###
-#findindex=lambda x:np.where(mt>=x)[0][0]
-#mindex=map(findindex,time)
-#print mindex
-
-# time_sim = [time[0]]
-# 
-# for i in range(1, len(time)):
-#     if time[i] < time[i-1]:
-#         break
-#     time_sim.append(time[i])
-
-# solver.run # saved in array - species y, obs in yobs, 
